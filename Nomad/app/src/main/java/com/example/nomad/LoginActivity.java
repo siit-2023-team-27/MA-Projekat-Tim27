a9da07a663aa7fd72ee83128e6ff7f367676e2cb
CodeX-cmd <kuvizic.marko@gmail.com>
2023-11-16 19:40:34 +0100

    [Add] Added layouts for register and login pages

--- Nomad/app/src/main/java/com/example/nomad/LoginActivity.java
+++ Nomad/app/src/main/java/com/example/nomad/LoginActivity.java
@@ -2,13 +2,38 @@ package com.example.nomad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button registerButton;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registerButton = findViewById(R.id.RegisterButton);
+        loginButton = findViewById(R.id.LoginButton);
+        setupLoginButton();
+        setupRegisterButton();
+
+    }
+    protected void setupLoginButton(){
+        loginButton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.putExtra("title", "Cart");
+            startActivity(intent);
+        });
+    }
+    protected void setupRegisterButton(){
+        registerButton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            intent.putExtra("title", "Cart");
+            startActivity(intent);
+        });
     }
 }
--- Nomad/app/src/main/java/com/example/nomad/RegisterActivity.java
+++ Nomad/app/src/main/java/com/example/nomad/RegisterActivity.java
@@ -2,13 +2,25 @@ package com.example.nomad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    Button nextButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        nextButton = findViewById(R.id.RegisterNextButton);
+        setupNextButton();
+    }
+    protected void setupNextButton(){
+        nextButton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(RegisterActivity.this, CreateAccountActivity.class);
+            intent.putExtra("title", "Cart");
+            startActivity(intent);
+        });
     }
 }
